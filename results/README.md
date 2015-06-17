24d15ab7fb1daf644aeb8b79427dae8e2ab89abf
Jack Hessel <jmhessel@gmail.com>
2015-06-17 15:38:35 -0400

    more work

---
+++ results/README.md
@@ -0,0 +1,3 @@
+# What goes here?
+
+This is a folder where results will be written. You don't need to put anything here!
--- setupExperiments.py
+++ setupExperiments.py
@@ -6,34 +6,61 @@ import numpy as np
 import json
 import sys
 import cPickle as pickle
+import json
 
-### SET THESE THINGS ###
-secondsPerTrial = 3600
-#this is how many train/val/test splits you have made
-numTrials = 10
-#this is the location of spearmint's main in your filesystem.
-#for me, this line is set as...
-#spearmintMain = "/usr/local/lib/python2.7/dist-packages/spearmint-0.1-py2.7.egg/spearmint/main.py"
-spearmintMain = ''
-########################
+with open('basicSpearmint.json') as f:
+    params = json.load(f)
 
-if len(spearmintMain) == 0:
-    print "Please set your spearmint main variable at the top of setupExperiments.py"
+spearmintMain = params['spearmintMain']
+numSplits = params['numSplits']
+secondsPerSplit = params['secondsPerSplit']
+
+print "Performing bayesian optimization over {} splits, with {} seconds per split".format(numSplits, secondsPerSplit)
+print "This will take a total of {} seconds.".format(numSplits*secondsPerSplit)
+time.sleep(1)
+
+print "Checking experiment setup..."
+
+#Check to make things are setup properly...
+if not os.path.exists(spearmintMain):
+    print "Please modify basicSpearmint.json's spearmintMain variable to point towards the location of spearmint's main!"
     quit()
 
+def checkExperimentFolder(directory):
+    files = os.listdir(directory)
+    neededFiles = ['train.npy','val.npy','test.npy',
+                   'trainLabel.npy', 'valLabel.npy','testLabel.npy']
+
+    if not set(neededFiles).issubset(set(files)):
+        print "Invalid experiment directory: " + directory
+        print "The directory must contain the following files..."
+        for n in neededFiles:
+            print n
+        quit()
+
+for i in range(numSplits):
+    if not os.path.exists('experiments/' + str(i)):
+        print "Experiment directory missing: experiments/" + str(i)
+        print "Please make sure that this directory exists!"
+        quit()
+    checkExperimentFolder('experiments/'+str(i))
+
+#### end formatting checks
 def decompress_array(a):
     return np.fromstring(zlib.decompress(a['value'].decode('base64'))).reshape(a['shape'])
 
-for i in range(numTrials):
-    os.system("cp experiment.py " + str(i))
-    os.system("cp config.json " + str(i))
+os.system('mongo spearmint --eval "db.dropDatabase()"')
+for i in range(numSplits):
+    os.system('mkdir .tempResults')
+    os.system("cp experiment.py experiments/" + str(i))
+    os.system("cp config.json experiments/" + str(i))
     p = subprocess.Popen(["python",spearmintMain,
-                          str(i)])
-    time.sleep(secondsPerTrial)
+                          'experiments/' + str(i)])
+    time.sleep(secondsPerSplit)
     p.kill()
-    os.system("mongoexport --db spearmint --collection basicSVMSpearmint.jobs --out tmp.jsons")
+    os.system("mongoexport --db spearmint --collection experiment.jobs --out .tempResults/tmp.jsons")
     runs = []
-    with open('tmp.jsons') as f:
+    with open('.tmp/tmp.jsons') as f:
         for line in f:
             runs.append(json.loads(line))
     
@@ -47,8 +74,8 @@ for i in range(numTrials):
         cOut['params'] = params
         cOut['objective'] = r['values']['main']
         output.append(cOut)
-    with open(str(i) + "/results.p", 'w') as f:
+    with open('results/' + str(i) + "_results.p", 'w') as f:
         pickle.dump(output, f)
         
-    os.system('rm tmp.jsons')
+    os.system('rm -rf .tempResults')
     os.system('mongo spearmint --eval "db.dropDatabase()"')
